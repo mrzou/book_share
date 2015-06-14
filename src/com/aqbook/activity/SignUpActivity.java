e99201250fce4e6ba033c0e9611e6a0cea874a8d
mrzouDom <1083203731@qq.com>
2015-06-15 01:02:21 +0800

    new update

--- src/com/aqbook/activity/SignUpActivity.java
+++ src/com/aqbook/activity/SignUpActivity.java
@@ -2,13 +2,13 @@ package com.aqbook.activity;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import cn.smssdk.EventHandler;
 import cn.smssdk.SMSSDK;
-import cn.smssdk.gui.RegisterPage;
-
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -19,24 +19,36 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.aqbook.R;
-
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.CountDownTimer;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SignUpActivity extends Activity {
+public class SignUpActivity<signUpButton> extends Activity {
 	
 	private RequestQueue signUpQueue;
+	private ProgressDialog mProgressDialog;
 	
 	EditText phonEditText;
+	EditText passwordEditText;
+	Button signUpButton;
+	Button button;
+	EditText passwordConfirmEditText;
 	
 	private static String APPKEY = "806cb6d938b4";
 	private static String APPSECRET = "5c627357ef651ecc7657974e4a235101";
@@ -48,7 +60,13 @@ public class SignUpActivity extends Activity {
 		setContentView(R.layout.activity_sign_up);
 		initSendMessageSdk();
 		
+		signUpQueue = Volley.newRequestQueue(this);
 		phonEditText = (EditText) findViewById(R.id.phone_edit);
+		passwordEditText = (EditText) findViewById(R.id.password_edit);
+		passwordConfirmEditText = (EditText) findViewById(R.id.confirm_password_edit);
+		signUpButton = (Button) findViewById(R.id.sign_up);
+		button = (Button) findViewById(R.id.sendValidCode);
+		
 		TextView textView1 = (TextView) findViewById(R.id.return_main_activity);
 		TextView textView2 = (TextView) findViewById(R.id.scan_book);
 		Typeface iconfont = Typeface.createFromAsset(getAssets(), "icon_little.ttf");
@@ -56,9 +74,101 @@ public class SignUpActivity extends Activity {
 		textView1.setTypeface(iconfont);
 		textView2.setTypeface(iconfont1);
 		
-		findViewById(R.id.sign_up);
+		setPhoneLostFocus();
+		validatePassword();
 	}
-	
+	//验证密码长度及是否一致
+	public void validatePassword(){
+		passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+			@Override
+			public void onFocusChange(View arg0, boolean arg1) {
+				// TODO Auto-generated method stub
+				if(!arg1){
+					if(passwordEditText.length()<6||passwordEditText.length()>12){
+						Toast.makeText(SignUpActivity.this, "密码长度不符合", Toast.LENGTH_SHORT).show();
+						if(signUpButton.isEnabled()){signUpButton.setEnabled(false);}
+					}else{
+						signUpButton.setEnabled(true);
+					}
+				}
+			}
+			
+		});
+		passwordConfirmEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+			@Override
+			public void onFocusChange(View arg0, boolean arg1) {
+				// TODO Auto-generated method stub
+				if(!arg1){
+					if(!passwordConfirmEditText.getText().toString().equals(passwordEditText.getText().toString())){
+						Toast.makeText(SignUpActivity.this, "确认密码不一样", Toast.LENGTH_SHORT).show();
+						if(signUpButton.isEnabled()){signUpButton.setEnabled(false);}
+					}else{
+						signUpButton.setEnabled(true);
+					}
+				}
+			}
+			
+		});
+	}
+	//电话失去焦点是监听是否有一致的号码
+	public void setPhoneLostFocus(){
+		phonEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {  
+		    @Override  
+		    public void onFocusChange(View v, boolean hasFocus) {  
+		        if(hasFocus){//获得焦点  
+		            //在这里可以对获得焦点进行处理  
+		        }else if(phonEditText.getText().toString().length()==11){//失去焦点
+		        	validatePhoneNumber(true);
+		        }else{
+		        	button.setEnabled(false);
+		        	Toast.makeText(SignUpActivity.this, "手机号长度不符合", 1).show();
+		        	if(signUpButton.isEnabled()){signUpButton.setEnabled(false);}
+		        }
+		    }             
+		});
+	}
+	//到后台去验证手机号码是否重复
+	public void validatePhoneNumber(final boolean state){
+		
+		button.setEnabled(true);
+    	Map<String, Object> phoneNumber = new HashMap<String, Object>();    
+		phoneNumber.put("phone_number", phonEditText.getText().toString());    
+		JSONObject jsonObject = new JSONObject(phoneNumber); 
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.POST,"http://192.168.1.106:3000/validate_messages/validatePhoneNumber", jsonObject,  
+	        new Response.Listener<JSONObject>() {  
+	            @Override  
+	            public void onResponse(JSONObject response) {  
+	            	signUpButton = (Button) findViewById(R.id.sign_up);
+	            	try {
+						if(response.get("state").equals("error")){
+							Toast.makeText(SignUpActivity.this, "手机号已注册", Toast.LENGTH_SHORT).show();
+							button.setEnabled(false);
+							if(signUpButton.isEnabled()){signUpButton.setEnabled(false);}
+						}else{
+							button.setEnabled(true);
+							signUpButton.setEnabled(true);
+							if(!state){
+								SMSSDK.getVerificationCode("86",phonEditText.getText().toString());	
+							}
+						}
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	            }  
+	        }, new Response.ErrorListener() {  
+	            @Override  
+	            public void onErrorResponse(VolleyError error) {  
+	                Log.v("TAG", error.getMessage(), error);  
+	            }  
+	        });  
+		int socketTimeout = 30000;//30 seconds - change to what you want
+		RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+		jsonObjectRequest.setRetryPolicy(policy);
+		signUpQueue.add(jsonObjectRequest);
+	}
 	public void click_to_back(View view){
 		this.onBackPressed();
 	}
@@ -80,71 +190,69 @@ public class SignUpActivity extends Activity {
             	   
 	               //回调完成
 	               if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
-	            	   Toast.makeText(SignUpActivity.this, "已发送验证码,请注意接收", Toast.LENGTH_SHORT).show();
 	               }else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE){
 	            	 //获取验证码成功
-	               }else if (event ==SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){
+	               }else if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){
 	               //返回支持发送验证码的国家列表
-	               } 
-              }else{                                                                 
-            	  ((Throwable)data).printStackTrace(); 
+	               }
               }
             } 
         }; 
         SMSSDK.registerEventHandler(eh); //注册短信回调
 	}
+	//点击验证码按钮
 	public void sendValidCode(View view){
-		
+		Log.v("TAG", "aaaaaaaaaaaa");
         String phString=phonEditText.getText().toString();
-		if(!TextUtils.isEmpty(phString)){
-			SMSSDK.getVerificationCode("86",phonEditText.getText().toString());
-		}else {
-			Toast.makeText(this, "电话不能为空", 1).show();
+        if(phString.length()==11){
+        	Log.v("TAG", "bbbbbbbbbbbbbb");
+        	button.setEnabled(true);
+        	validatePhoneNumber(false);
+		}else{
+			Toast.makeText(this, "电话长度不正确", 1).show();
+			button.setEnabled(false);
 		}
-
 	}
-//	注册
-	public void clickToSignUp(View view) {
-		validateCode(phonEditText.getText().toString());
-		
-		signUpQueue = Volley.newRequestQueue(this);
-		Map<String, Object> map = new HashMap<String, Object>();    
-		map.put("email", "12345678@x.com");    
-		map.put("password", "12345678");    
-		JSONObject jsonObject = new JSONObject(map); 
-		Map<String, Object> allMap = new HashMap<String, Object>();    
-		allMap.put("user", jsonObject);
-		JSONObject jsonObjectAll = new JSONObject(allMap); 
-		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.POST,"http://192.168.1.106:3000/users/sign_in", jsonObjectAll,  
-		        new Response.Listener<JSONObject>() {  
-		            @Override  
-		            public void onResponse(JSONObject response) {  
-		                Log.d("TAG", response.toString());  
-		            }  
-		        }, new Response.ErrorListener() {  
-		            @Override  
-		            public void onErrorResponse(VolleyError error) {  
-		                Log.e("TAG", error.getMessage(), error);  
-		            }  
-		        });  
-		int socketTimeout = 30000;//30 seconds - change to what you want
-		RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
-		jsonObjectRequest.setRetryPolicy(policy);
-		signUpQueue.add(jsonObjectRequest);
+	//注册
+	public void clickToSignUp(View view) throws TimeoutException {
+		EditText acount_name = (EditText) findViewById(R.id.acount_edit);
+		EditText validEdit = (EditText) findViewById(R.id.valid_edit);
+		if(acount_name.getText().toString().equals("")){
+			Toast.makeText(this, "用户名不能为空", 1).show();
+			signUpButton.setEnabled(false);
+		}else if(!validEdit.getText().toString().equals("")){
+			validateCode(phonEditText.getText().toString());
+			
+		}else{
+			Toast.makeText(SignUpActivity.this, "验证码不能为空", 1).show();
+		}
 	}
+	
 	//发送确认的验证码
-	public void validateCode(String number){
+	public void validateCode(String number) throws TimeoutException{
 		EditText validEdit = (EditText) findViewById(R.id.valid_edit);
 		if(TextUtils.isEmpty(validEdit.getText().toString())){
 			Toast.makeText(this, "验证码不能为空", 1).show();
+			if(signUpButton.isEnabled()){signUpButton.setEnabled(false);}
 		}else{
+			signUpButton.setEnabled(true);
 			String params = "appkey=806cb6d938b4&phone="+number+"&zone=86"+"&code="+validEdit.getText().toString();
 			
 			StringRequest stringRequest = new StringRequest("https://api.sms.mob.com/sms/verify?"+params, 
 					new Response.Listener<String>() {  
 			            @Override  
 			            public void onResponse(String response) {  
-			                Log.v("TAG", response);  
+			            	try {
+								JSONObject stringToJson = new JSONObject(response);
+								if(!stringToJson.get("status").toString().equals("200")){
+									Toast.makeText(SignUpActivity.this, "验证码错误", Toast.LENGTH_SHORT).show();
+								}else{
+									excuteSignUp();
+								}
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 			            }  
 			        }, new Response.ErrorListener() {  
 			            @Override  
@@ -152,11 +260,74 @@ public class SignUpActivity extends Activity {
 			                Log.v("TAG", error.getMessage(), error);  
 			            }  
 			        });
-			int socketTimeout = 30000;//30 seconds - change to what you want
+			int socketTimeout = 3000;//30 seconds - change to what you want
 			RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
 			stringRequest.setRetryPolicy(policy);
-			signUpQueue.add(stringRequest);
+			synchronized (signUpQueue) { signUpQueue.add(stringRequest); }
 		}
+	}
+	//验证码正确后执行的注册功能
+	public void excuteSignUp(){
+		showProgressDialog();
+		EditText acount_name = (EditText) findViewById(R.id.acount_edit);
+		signUpButton.setEnabled(true);
+		EditText password_confirm = (EditText)findViewById(R.id.confirm_password_edit);
+		Map<String, Object> map = new HashMap<String, Object>();    
+		map.put("phone_number", phonEditText.getText().toString());    
+		map.put("password", passwordEditText.getText().toString());    
+		map.put("password_confirmation", password_confirm.getText().toString());    
+		map.put("name", acount_name.getText().toString());    
+		JSONObject jsonObject = new JSONObject(map); 
+		Map<String, Object> allMap = new HashMap<String, Object>();    
+		allMap.put("user", jsonObject);
+		JSONObject jsonObjectAll = new JSONObject(allMap); 
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.POST,"http://192.168.1.106:3000/users.json", jsonObjectAll,  
+			new Response.Listener<JSONObject>() {  
+				@Override  
+				public void onResponse(JSONObject response) {  
+					try {
+						if(response.get("state").toString().equals("error")){
+							Toast.makeText(SignUpActivity.this, "注册失败!", 1).show();
+						}else{
+							mProgressDialog.cancel();
+							Toast.makeText(SignUpActivity.this, "注册成功!", 1).show();
+							String token = response.get("token").toString();
+							String user_id = response.get("user_id").toString();
+							saveTokenToPreference(token, user_id);
+							SignUpActivity.this.onBackPressed();
+						}
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						Toast.makeText(SignUpActivity.this, "注册错误!", 1).show();
+					}  
+				}  
+			}, new Response.ErrorListener() {  
+				@Override  
+				public void onErrorResponse(VolleyError error) {  
+					Log.v("TAG", error.getMessage(), error);  
+				}  
+		});
+		int socketTimeout = 30000;//30 seconds - change to what you want
+		RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+		jsonObjectRequest.setRetryPolicy(policy);
+		signUpQueue.add(jsonObjectRequest);
+	}
+	//保存token的消息
+	public void saveTokenToPreference(String token, String user_id){
+		SharedPreferences.Editor editor = getSharedPreferences("token", MODE_PRIVATE).edit();
+		editor.putString("token", token);
+		editor.putString("user_id", user_id);
+		editor.commit();
+	}
+	// 开启注册进度条
+	private void showProgressDialog() {
+		mProgressDialog = new ProgressDialog(this);
+		mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		mProgressDialog.setMessage("注册中...");
+		mProgressDialog.setIndeterminate(false);
+		mProgressDialog.setCancelable(false);
+		mProgressDialog.show();
 	}
 	@Override
 	protected void onDestroy() {
@@ -173,8 +344,6 @@ public class SignUpActivity extends Activity {
 			super.handleMessage(msg);
 			int event = msg.arg1;
 			int result = msg.arg2;
-			Object data = msg.obj;
-			Log.e("TAG", "event="+event);
 			if (result == SMSSDK.RESULT_COMPLETE) {
 				//短信注册成功后，返回MainActivity,然后提示新好友
 				if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {//提交验证码成功
@@ -184,11 +353,35 @@ public class SignUpActivity extends Activity {
 					Toast.makeText(getApplicationContext(), "验证码已经发送", Toast.LENGTH_SHORT).show();
 //					textView2.setText("验证码已经发送");
 				}
+				new CountDownTimer(60000, 1000) {
+
+				     public void onTick(long millisUntilFinished) {
+				    	 button.setEnabled(false);
+				         button.setText("重新获取("+String.valueOf(millisUntilFinished / 1000)+"s)");
+				     }
+
+				     public void onFinish() {
+				         button.setText("重新获取");
+				         button.setEnabled(true);
+				     }
+				  }.start();
+			}else{
+				Toast.makeText(getApplicationContext(), "手机号码错误!", 2).show();
 			}
 			
 		}
 		
 	};
-
-
+	//点击布局空白处可去掉Edittext的焦点，同时收缩输入键盘
+	//@Override
+    public boolean onTouchEvent(MotionEvent event) {
+		LinearLayout linearLayout = (LinearLayout) findViewById(R.id.sigUpAllLayout);
+	    InputMethodManager imm = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+	    //login_auto_account.setCursorVisible(false);
+	    linearLayout.setFocusable(true);
+        linearLayout.setFocusableInTouchMode(true);
+        linearLayout.requestFocus();
+	    imm.hideSoftInputFromWindow(phonEditText.getWindowToken(), 0); 
+        return super.onTouchEvent(event);
+    }
 }
--- src/com/aqbook/activity/fragment/FourFragment.java
+++ src/com/aqbook/activity/fragment/FourFragment.java
@@ -1,8 +1,28 @@
 package com.aqbook.activity.fragment;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.Request.Method;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.aqbook.R;
+import com.aqbook.activity.LoadingActivity;
+import com.aqbook.activity.MainActivity;
 import com.aqbook.activity.SignUpActivity;
+
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,30 +30,146 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class FourFragment extends Fragment {
+public class FourFragment extends Fragment implements OnClickListener{
 
+	View view;
+	Button signOutView;
+	TextView signUpButton;
+	Button signInButton;
+	private RequestQueue signInQueue;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_four, null);
-		TextView textView = (TextView) view.findViewById(R.id.logUpUser);
-		textView.setOnClickListener(new OnClickListener(){
-
+		
+		signInQueue = Volley.newRequestQueue(getActivity());
+		testIfToken(inflater);
+		return view;
+	}
+	//登陆注册按钮
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.logUpUser:
+			Intent intent = new Intent(getActivity(), SignUpActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.btn_login:
+			userLogin();
+			break;
+		}
+	}
+	//手机　密码检测
+	public void userLogin(){
+		String userPhone = ((EditText) view.findViewById(R.id.acount_edit)).getText().toString();
+		String userPassword = ((EditText) view.findViewById(R.id.password_edit)).getText().toString();
+		int passwordLength = userPassword.length();
+		if(userPhone.length() != 11){
+			Toast.makeText(getActivity(), "电话号码不正确", 1).show();
+		}else if(passwordLength<6||passwordLength>13){
+			Toast.makeText(getActivity(), "密码长度错误", 1).show();
+		}else{
+			userToLogin(userPhone, userPassword);
+		}
+	}
+	//提交到后台
+	public void userToLogin(String userPhone, String userPassword){
+		Map<String, Object> map = new HashMap<String, Object>();    
+		map.put("phone_number", userPhone);    
+		map.put("password", userPassword);    
+		JSONObject jsonObject = new JSONObject(map); 
+		Map<String, Object> allMap = new HashMap<String, Object>();    
+		allMap.put("user", jsonObject);
+		JSONObject jsonObjectAll = new JSONObject(allMap); 
+		JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Method.POST,"http://192.168.1.106:3000/users/sign_in", jsonObjectAll,  
+			new Response.Listener<JSONObject>() {  
+				@Override  
+				public void onResponse(JSONObject response) {  
+					try {
+						if(!response.get("state").toString().equals("error")){
+							SharedPreferences.Editor editor = getActivity().getSharedPreferences("token", 0).edit();
+							editor.putString("token", response.get("token").toString());
+							editor.putString("user_id", response.get("user_id").toString());
+							editor.commit();
+							
+							Intent intent = new Intent();  
+					        intent.setClass(getActivity(),LoadingActivity.class);//跳转到加载界面  
+					        startActivity(intent); 
+						}else{
+							
+						}
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						Toast.makeText(getActivity(), "登陆错误!", 1).show();
+					}  
+				}  
+			}, new Response.ErrorListener() {  
+				@Override  
+				public void onErrorResponse(VolleyError error) {  
+					Toast.makeText(getActivity(), "密码错误!", 1).show();
+				}  
+		});
+		int socketTimeout = 30000;//30 seconds - change to what you want
+		RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+		jsonObjectRequest.setRetryPolicy(policy);
+		signInQueue.add(jsonObjectRequest);
+	}
+	//根据是否有token决定是否显示用户信息
+	public void testIfToken(LayoutInflater inflater){
+		SharedPreferences token = getActivity().getSharedPreferences("token", 0);
+		if(token.getString("token", "").equals("")){
+			view = inflater.inflate(R.layout.fragment_four, null);
+			signUpButton = (TextView) view.findViewById(R.id.logUpUser);
+			signInButton = (Button) view.findViewById(R.id.btn_login);
+			signInButton.setOnClickListener(this);
+			signUpButton.setOnClickListener(this);
+		}else{
+			view = inflater.inflate(R.layout.personal_message, null);
+			setPicture();
+		}
+	}
+	public void setPicture(){
+		TextView already_come_out = (TextView) view.findViewById(R.id.already_come_out);
+		TextView collect_book = (TextView) view.findViewById(R.id.collect_book);
+		TextView goto_already_come_out = (TextView) view.findViewById(R.id.goto_already_come_out);
+		TextView goto_collect_book = (TextView) view.findViewById(R.id.goto_collect_book);
+		signOutView = (Button) view.findViewById(R.id.sign_out);
+		Typeface iconfont = Typeface.createFromAsset(getActivity().getAssets(), "icon_little.ttf");
+		Typeface sign_out = Typeface.createFromAsset(getActivity().getAssets(), "sign_out.ttf");
+		already_come_out.setTypeface(iconfont);
+		collect_book.setTypeface(iconfont);
+		goto_already_come_out.setTypeface(iconfont);
+		goto_collect_book.setTypeface(iconfont);
+		signOutView.setTypeface(sign_out);
+		signOutListener();
+	}
+	//注册登出事件
+	public void signOutListener(){
+		signOutView.setOnClickListener(new OnClickListener(){
+	
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				Log.v("TAG", "excute clickToLogUp");
-				Intent intent = new Intent(getActivity(), SignUpActivity.class);
-				startActivity(intent);
-				
+				SharedPreferences.Editor editor = getActivity().getSharedPreferences("token", 0).edit();
+				editor.putString("token", "");
+				editor.putString("user_id", "");
+				editor.commit();
+				Toast.makeText(getActivity(), "用户退出成功", 1).show();
+				/*MainActivity mainActivity = (MainActivity) getActivity();
+				mainActivity.setContentView(R.layout.fragment_four);*/
 			}
-			
 		});
-		return view;
+	}
+	@Override
+	public void onDetach(){
+		super.onDetach();
+		Log.v("TAG", "onDetach");
 	}
 	
-	public void clickToLogUp(View view){
-	}
 }
